bebb289e6b55b54c5fe94f37cd837026fb28b55c
GabrielM0rais <revoltabrg@gmail.com>
2021-05-09 21:44:49 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,12 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/request";
 
 function DataTable() {
+  const [activePage, setPageActive] = useState(0);
   const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
@@ -14,24 +16,30 @@ function DataTable() {
   });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date`).then((response) => {
+    axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date`).then((response) => {
       setPage(response.data);
     });
-  }, []);
+  }, [activePage]);
+
+  const changePage = (index: number) => {
+    setPageActive(index);
+  };
 
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
+    <>
+      <Pagination page={page} onPageChange={changePage}/>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
             {page.content?.map((item) => (
               <tr key={item.id}>
                 <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
@@ -41,9 +49,10 @@ function DataTable() {
                 <td>{item.amount.toFixed(2)}</td>
               </tr>
             ))}
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
